5c590a235e75d440bb2308e182d2010e44ee571c
Eddie <edward.washington10@gmail.com>
2013-05-14 02:06:35 -0400

    Finished

--- director.rb
+++ director.rb
@@ -1,29 +1,24 @@
-require 'json'
-require 'rest-client'
 require_relative 'parser'
 
 
-class Director
-
-	Parser.new
-
-
+def get_director
 		puts "Enter your favorite movie and we'll tell you the Director!"
-		input = gets.chomp
+		@input = gets.chomp
 
-		if input.include? "&"
-			input= input.gsub("&","%26")
+		if @input.include? "&"
+			@input = @input.gsub("&","%26")
 		end
-		input = input.gsub(" ", "+")
+		@input = @input.gsub(" ", "+")
 
-		get_movie(input)
+		new_movie = Parser.new(@input)
+		new_movie.get_movie
 
-		if movie["Title"] == nil
-			puts "This movie doesn't exist!"
+		if new_movie.key["Title"] == nil
+			puts new_movie.key["Error"]
 			puts "Please enter another movie!"
 		else
-			puts "The Director for #{movie["Title"]} is #{movie["Director"]}"
+			puts "The Director for #{new_movie.key["Title"]} is #{new_movie.key["Director"]}"
 		end
 end
 
-Director.new
+get_director
